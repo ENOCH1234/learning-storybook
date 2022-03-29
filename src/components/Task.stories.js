376d81fd198040842a1013a8f4528eb9332961d9
Kayode Oluwasegun <segunkayode00@gmail.com>
2022-03-29 14:59:55 +0100

    new change in stories

--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -13,7 +13,7 @@ export const Default = Template.bind({});
 Default.args = {
   task: {
     id: '1',
-    title: 'Test Task',
+    title: 'Some test',
     state: 'TASK_INBOX',
     updatedAt: new Date(2021, 0, 1, 9, 0),
   },
